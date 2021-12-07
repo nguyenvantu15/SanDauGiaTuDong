d3d9b5b4c1ab6ab28d0af1845297fe071aba0beb
truyen <nguyenvantruyennvt@gmail.com>
2021-12-07 23:16:48 +0700

    Xây dựng chức năng thêm user

--- src/main/java/com/ute/sandaugiatudong/controllers/AccountServlet.java
+++ src/main/java/com/ute/sandaugiatudong/controllers/AccountServlet.java
@@ -13,43 +13,43 @@ import java.io.IOException;
 public class AccountServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        {
-            String path = request.getPathInfo();
-
-            switch (path) {
-                case "/Register" :
-                    ServletUtils.forward("/views/vwAccount/Register.jsp",request,response);
-                    break;
-                case "/Login" :
-                    ServletUtils.forward("/views/vwAccount/Login.jsp",request,response);
-                    break;
-                case "/Profile" :
-                    ServletUtils.forward("/views/vwAccount/Profile.jsp",request,response);
-                    break;
-                default:
-                    //ServletUtils.forward("/views/404.jsp",request,response);
-                    break;
-            }
-        }
+//        {
+//            String path = request.getPathInfo();
+//
+//            switch (path) {
+//                case "/Register" :
+//                    ServletUtils.forward("/views/vwAccount/Register.jsp",request,response);
+//                    break;
+//                case "/Login" :
+//                    ServletUtils.forward("/views/vwAccount/Login.jsp",request,response);
+//                    break;
+//                case "/Profile" :
+//                    ServletUtils.forward("/views/vwAccount/Profile.jsp",request,response);
+//                    break;
+//                default:
+//                    //ServletUtils.forward("/views/404.jsp",request,response);
+//                    break;
+//            }
+//        }
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String path = request.getPathInfo();
-        switch (path){
-
-            //post Register
-            case "/Register":
-                registerUser(request,response);
-                break;
-
-            //post Login
-            case "/Login":
-                break;
-            default:
-                //ServletUtils.forward("/views/404.jsp",request,response);
-                break;
-        }
+//        String path = request.getPathInfo();
+//        switch (path){
+//
+//            //post Register
+//            case "/Register":
+//                registerUser(request,response);
+//                break;
+//
+//            //post Login
+//            case "/Login":
+//                break;
+//            default:
+//                //ServletUtils.forward("/views/404.jsp",request,response);
+//                break;
+//        }
     }
     protected void registerUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
