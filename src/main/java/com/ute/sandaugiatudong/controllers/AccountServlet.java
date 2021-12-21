e304e40f9ea6b584fd2af143a3210a280a8f76a9
truyen <nguyenvantruyennvt@gmail.com>
2021-12-21 22:13:02 +0700

    Chỉnh Tiếng Việt

--- src/main/java/com/ute/sandaugiatudong/controllers/AccountServlet.java
+++ src/main/java/com/ute/sandaugiatudong/controllers/AccountServlet.java
@@ -105,6 +105,7 @@ public class AccountServlet extends HttpServlet {
     }
 
     private void registerUser(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setCharacterEncoding("UTF-8");
 
         String username = request.getParameter("username");
 
@@ -133,6 +134,8 @@ public class AccountServlet extends HttpServlet {
 
     private void updatePro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //Lay du lieu tren view xuong
+        request.setCharacterEncoding("UTF-8");
+
         int id =Integer.parseInt( request.getParameter("id"));
         String name = request.getParameter("name");
         String email = request.getParameter("email");
