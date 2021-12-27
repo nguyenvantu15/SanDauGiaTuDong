8ee4c7d47ac0900983b2871a4af07d48d16c2723
tú nguyễn <19110035@student.hcmute.edu.vn>
2021-12-27 17:57:15 +0700

    thêm chức năng review seller

--- src/main/java/com/ute/sandaugiatudong/controllers/ReviewServlet.java
+++ src/main/java/com/ute/sandaugiatudong/controllers/ReviewServlet.java
@@ -3,6 +3,7 @@ package com.ute.sandaugiatudong.controllers;
 import com.ute.sandaugiatudong.beans.Category;
 import com.ute.sandaugiatudong.beans.Product;
 import com.ute.sandaugiatudong.beans.Type;
+import com.ute.sandaugiatudong.beans.User;
 import com.ute.sandaugiatudong.models.ProductModels;
 import com.ute.sandaugiatudong.models.TypeModels;
 import com.ute.sandaugiatudong.utils.ServletUtils;
@@ -36,6 +37,33 @@ public class ReviewServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String path = request.getPathInfo();
+        switch (path) {
+            case "/Reviewseller":
+                int ProID = Integer.parseInt(request.getParameter("id"));
+                Product product = ProductModels.findById(ProID);
+                if(product == null){
+                    ServletUtils.forward("/views/404.jsp", request, response);
+                    break;
+                }
+                int idSeller = product.getIdUserSell();
 
+                HttpSession session = request.getSession();
+                User u = (User) session.getAttribute("authUser");
+
+                String like = request.getParameter("likeSeller");
+
+                System.out.println(like);
+
+                String url = request.getHeader("referer");
+                if (url == null) url = "/Home";
+                ServletUtils.redirect(url, request, response);
+                break;
+            case "/Reviewbidder":
+                break;
+            default:
+                ServletUtils.forward("/views/404.jsp", request, response);
+                break;
+        }
     }
 }
