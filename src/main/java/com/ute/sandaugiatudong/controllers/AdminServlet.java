6e460deeddec230360bc931f76f78537e774b55b
truyen <nguyenvantruyennvt@gmail.com>
2021-12-28 13:13:37 +0700

    Chỉnh sửa gửi mail thông báo resetpass

--- src/main/java/com/ute/sandaugiatudong/controllers/AdminServlet.java
+++ src/main/java/com/ute/sandaugiatudong/controllers/AdminServlet.java
@@ -173,6 +173,7 @@ public class AdminServlet extends HttpServlet {
         prop.put("mail.smtp.port","587");
         prop.put("mail.smtp.auth","true");
         prop.put("mail.smtp.starttls.enable","true");
+        prop.put("mail.smtp.ssl.protocols", "TLSv1.2");
         Session session = Session.getInstance(prop,new javax.mail.Authenticator(){
             protected  PasswordAuthentication getPasswordAuthentication(){
                 return new PasswordAuthentication(sendMail,passSendMail);
